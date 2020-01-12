95acac32ed264003df53e4cb3a2bc3c11337f373
h4sohail <hamza.sohail@carleton.ca>
2020-01-12 01:34:31 -0500

    Add concurrency

--- fetcher.py
+++ fetcher.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import sys, argparse
+import sys, argparse, time
 import tweepy
 import mysql.connector
-
 import unicodedata
 from unidecode import unidecode
+
+
+temp_tweets = []
+
 
 def string_cleaner(input_string):
     return_string = ""
@@ -25,6 +28,7 @@ def string_cleaner(input_string):
                      return_string += "[x]"
 
     return return_string
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -61,56 +65,55 @@ def main():
     max_tweets = 10000000 
 
     tweet_count = 0
-    print(f'Downloading max {max_tweets} tweets')
-    while tweet_count < max_tweets:
-        try:
-            if (max_id <= 0):
-                if (not since_id):
-                    new_tweets = api.search(q=search_query, count=tweets_per_query)
+    while(True):
+        while tweet_count < max_tweets:
+            try:
+                if (max_id <= 0):
+                    if (not since_id):
+                        new_tweets = api.search(q=search_query, count=tweets_per_query)
+                    else:
+                        new_tweets = api.search(q=search_query, count=tweets_per_query,
+                                                since_id=since_id)
                 else:
-                    new_tweets = api.search(q=search_query, count=tweets_per_query,
-                                            since_id=since_id)
-            else:
-                if (not since_id):
-                    new_tweets = api.search(q=search_query, count=tweets_per_query,
-                                            max_id=str(max_id - 1))
-                else:
-                    new_tweets = api.search(q=search_query, count=tweets_per_query,
-                                            max_id=str(max_id - 1),
-                                            since_id=since_id)
-            if not new_tweets:
-                print('No more tweets found')
+                    if (not since_id):
+                        new_tweets = api.search(q=search_query, count=tweets_per_query,
+                                                max_id=str(max_id - 1))
+                    else:
+                        new_tweets = api.search(q=search_query, count=tweets_per_query,
+                                                max_id=str(max_id - 1),
+                                                since_id=since_id)
+                if not new_tweets:
+                    break
+
+                for tweet in new_tweets:
+                    tweet_count += 1
+                    tweet_text = tweet._json['text']
+                    tweet_user = tweet._json['user']['screen_name']
+                    tweet_pfp = tweet._json['user']['profile_image_url']
+                    tweet_date = tweet._json['created_at']
+                    try:
+                        tweet_text.encode('latin1')
+                        
+                        sql = 'INSERT INTO tweets (username, user_avatar, tweet_content, date_posted) VALUES (%s, %s, %s, %s)'
+                        val = (tweet_user, tweet_pfp, tweet_text, tweet_date)
+                        mycursor.execute(sql, val)
+                        mydb.commit()
+
+                    except UnicodeEncodeError:
+                        tweet_text = string_cleaner(tweet_text)
+                        sql = 'INSERT INTO tweets (username, user_avatar, tweet_content, date_posted) VALUES (%s, %s, %s, %s)'
+                        val = (tweet_user, tweet_pfp, tweet_text, tweet_date)
+                        mycursor.execute(sql, val)
+                        mydb.commit()
+
+                max_id = new_tweets[-1].id
+            except tweepy.TweepError as e:
+                # Just exit if any error
+                print('some error : ' + str(e))
                 break
-            for tweet in new_tweets:
-                tweet_count += 1
-                tweet_text = tweet._json['text']
-                tweet_user = tweet._json['user']['screen_name']
-                tweet_pfp = tweet._json['user']['profile_image_url']
-                tweet_date = tweet._json['created_at']
-                try:
-                    tweet_text.encode('latin1')
-                    
-                    sql = 'INSERT INTO tweets (username, user_avatar, tweet_content, date_posted) VALUES (%s, %s, %s, %s)'
-                    val = (tweet_user, tweet_pfp, tweet_text, tweet_date)
-                    mycursor.execute(sql, val)
-                    mydb.commit()
-                except UnicodeEncodeError:
-                    print('stop using emotes :-)')
 
-                    tweet_text = string_cleaner(tweet_text)
-                    sql = 'INSERT INTO tweets (username, user_avatar, tweet_content, date_posted) VALUES (%s, %s, %s, %s)'
-                    val = (tweet_user, tweet_pfp, tweet_text, tweet_date)
-                    mycursor.execute(sql, val)
-                    mydb.commit()
-            
-            print(f'Downloaded {tweet_count} tweets')
-            max_id = new_tweets[-1].id
-        except tweepy.TweepError as e:
-            # Just exit if any error
-            print('some error : ' + str(e))
-            break
+        time.sleep(300)
 
-    print (f'Downloaded {tweet_count} tweets')
 
 if __name__ == '__main__':
     main()
