92a5486cdab37a0c1654ea1054ee53e14cbae665
helloMusa <esquitoban@gmail.com>
2020-01-12 07:24:28 -0500

    Add good/bad tweet count to home route

--- analyzer/routes.py
+++ analyzer/routes.py
@@ -7,15 +7,14 @@ from analyzer.twitter_funcs import *
 def home():
 	tweets = get_tweets()
 	tweets_count = get_recent_tweets_count(tweets)
-	tweets_good = are_tweets_good(tweets)
+	tweets_good, good_tweets_count, bad_tweets_count = get_tweet_sentiment(tweets)
 
 	if tweets_good == True:
 		text_type = "text-success"
 	else:
 		text_type = "text-danger"
 
-
-	return render_template('home.html', tweets_count=tweets_count, text_type=text_type)
+	return render_template('home.html', tweets_count=tweets_count, text_type=text_type, good_count=good_tweets_count, bad_count=bad_tweets_count)
 
 
 @app.route("/tweets")
