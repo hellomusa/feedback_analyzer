9c111daa377f3b5a7481abb1d64308a423aa5d83
helloMusa <esquitoban@gmail.com>
2020-01-12 03:50:39 -0500

    Add tweet counter function

--- analyzer/routes.py
+++ analyzer/routes.py
@@ -3,16 +3,10 @@ from analyzer import app
 import argparse
 import mysql.connector
 
-@app.route("/")
-def home():
-	return render_template('home.html')
 
-@app.route("/tweets")
-def tweets():
+def get_tweets():
 	parser = argparse.ArgumentParser()
-
 	parser.add_argument('sql_password', help='SQL database password')
-
 	args = parser.parse_args()
 
 	mydb = mysql.connector.connect(
@@ -27,6 +21,31 @@ def tweets():
 	mycursor.execute(sql_select_query)
 	tweets = mycursor.fetchall()
 
+	return tweets
+
+
+def get_recent_tweets_count(tweets):
+	counter = 0
+	current_month = tweets[0][4][4:8] # Ex. "Jan"
+	current_day = int(tweets[0][4][8:10])
+	for tweet in tweets:
+		tweet_month = tweet[4][4:8]
+		tweet_day = int(tweet[4][8:10])
+		if (tweet_month == current_month) and (tweet_day >= current_day - 5 and tweet_day <= current_day):
+			counter += 1
+
+	return counter
+
+
+@app.route("/")
+def home():
+	tweets = get_tweets()
+	tweets_count = get_recent_tweets_count(tweets)
+	return render_template('home.html', tweets_count=tweets_count)
+
+@app.route("/tweets")
+def tweets():
+	tweets = get_tweets()
 	return render_template('tweets.html', tweets=tweets)
 
 @app.route("/login")
